de25bcd502db94cb0a2ea44c8dc0f1345df3912e
maheshak100 <maher.shaker@live.se>
2021-07-29 20:39:11 +0200

    page stuff

---
+++ page/www/pkg/calculator_wasm.d.ts
@@ -0,0 +1,11 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {string} name
+*/
+export function greet(name: string): void;
+/**
+* @param {string} data
+* @returns {string}
+*/
+export function parse_input(data: string): string;
